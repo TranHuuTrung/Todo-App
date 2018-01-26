a19d817b310d1450ebbd51e8d4c7d9122bb0eed8
TranHuuTrung <huutrungbkdn15@gmail.com>
2018-01-26 22:56:07 +0700

    fix validate for register and login

--- style/js/login.js
+++ style/js/login.js
@@ -3,34 +3,37 @@ function directHomeUser(){
      document.location.href = "./home.html";
 }
 function postLogin(){
-    var email_login = $("#email").val();
-    var pass_login  = $("#password").val();
-    $(".loading").html("<i class='fa fa-spinner fa-pulse fa-3x fa-fw'></i>");
+    if($("form#login-form").valid()){
 
-    var request = $.ajax({
-        type: 'POST',
-        url: "https://todo-js-be.herokuapp.com/auth/sign_in",
-        data:{
-            'email'    : email_login,
-            'password' : pass_login
-        }
-    });
-    request.done(function(data, textStatus, jqXHR){
-        var uid         = jqXHR.getResponseHeader("Uid");
-        var accessToken = jqXHR.getResponseHeader("Access-Token");
-        var client      = jqXHR.getResponseHeader("Client");
+        var email_login = $("#email").val();
+        var pass_login  = $("#password").val();
 
-        localStorage.uid = uid;
-        localStorage.accessToken = accessToken;
-        localStorage.client = client;
-        $(".loading").children().remove();
-        //load vao trang home of user have account
-        directHomeUser();
-    });
-    request.fail(function(){
-        console.log("Error");
-        $(".loading").children().remove();
-        alert("Thông tin đăng nhập bị sai!");
-    });
+        $(".loading").html("<i class='fa fa-spinner fa-pulse fa-3x fa-fw'></i>");
 
+        var request = $.ajax({
+            type: 'POST',
+            url: "https://todo-js-be.herokuapp.com/auth/sign_in",
+            data:{
+                'email'    : email_login,
+                'password' : pass_login
+            }
+        });
+        request.done(function(data, textStatus, jqXHR){
+            var uid         = jqXHR.getResponseHeader("Uid");
+            var accessToken = jqXHR.getResponseHeader("Access-Token");
+            var client      = jqXHR.getResponseHeader("Client");
+
+            localStorage.uid = uid;
+            localStorage.accessToken = accessToken;
+            localStorage.client = client;
+            $(".loading").children().remove();
+            //load vao trang home of user have account
+            directHomeUser();
+        });
+        request.fail(function(){
+            console.log("Error");
+            $(".loading").children().remove();
+            alert("Thông tin đăng nhập bị sai!");
+        });
+    }
 }
--- style/js/register.js
+++ style/js/register.js
@@ -1,38 +1,14 @@
     function directHome(){
         $(".content").load("./snippets/start.html");
     }
+    
     function postSignUp(){
-        var firstname_user = $("#firstname").val();
-        var lastname_user  = $("#lastname").val();
-        var email_user     = $("#email").val();
-        var password_user  = $("#password").val();
-        var verifypass_user= $("#verify_password").val();
-        
-        function Valid_info(){
-            if(firstname_user == ''){
-                $("div.invalid-firstname").html('Please fill in firstname !');
-                return false;
-            }
-            if(lastname_user == ''){
-                $("div.invalid-lastname").html('Please fill in lastname !');
-                return false;
-            }
-            if(email_user == ''){
-                $("div.invalid-email").html('Please fill in email !');
-                return false;
-            }
-            if(password_user == ''){
-                $("div.invalid-pass").html('Please fill in password !');
-                return false;
-            }
-            if(verifypass_user == '' || password_user != verifypass_user){
-                $("div.invalid-verify-pass").html('Please fill in verify password !');
-                return false;
-            }
-            return true;
-        }
-        
-        if(Valid_info()){
+        if($("form#form-register-validate").valid()){
+            var firstname_user = $("#firstname").val();
+            var lastname_user  = $("#lastname").val();
+            var email_user     = $("#email").val();
+            var password_user  = $("#password").val();
+            var verifypass_user= $("#verify_password").val();
             $(".loading").html("<i class='fa fa-spinner fa-pulse fa-3x fa-fw'></i>");
             var result = $.ajax({
                 type: 'POST',
@@ -61,34 +37,4 @@
                 alert("Email already exists !");
             });
         }
-        else{
-        //    alert("");
-       }
-        // $(".loading").html("<i class='fa fa-spinner fa-pulse fa-3x fa-fw'></i>");
-        // console.log(email_user);
-        // console.log(password_user);
-        // var result = $.ajax({
-        //     type: 'POST',
-        //     crossDomain: true,
-        //     url : "https://todo-js-be.herokuapp.com/auth",
-        //     data: {
-        //         'email'   : email_user,
-        //         'password': password_user
-        //     }
-        // });
-        // result.done(function(data){
-        //    console.log(data);
-        //    $("div.form-register").hide();
-        //    $(".loading").children().remove();
-        //    $('div.success-regis').html('<div class="alert alert-success" style="width:200px; margin: 50px auto; " role="alert">Đăng ký thành công !</div>');
-        //    setTimeout(function(){
-        //       directHome();
-        //       $('div.success-regis').hide();
-        //    },1000);
-        // });
-        // result.fail(function(jqXHR, textStatus){
-        //     console.log("eror");
-        //     $(".loading").children().remove();
-        //     alert("Thông tin nhập bị sai hoặc email bị trùng!");
-        // });
-    };
+    }
